dad4b92e45ce14b658b2126e12754938ab56a5c5
rsetter <52512382+rsetter@users.noreply.github.com>
2021-04-13 16:19:37 -1000

    Add files via upload

--- whaledata.R
+++ whaledata.R
@@ -5,36 +5,61 @@ library(gdal)
 setwd("F:/whales")
 
 
-###avhrr
-
-#download the avhrr files
-urlsfeb <- XML::getHTMLLinks(
-  "https://www-ncei-noaa-gov.eres.library.manoa.hawaii.edu/data/oceans/pathfinder/Version5.3/L3C/1985/data/", 
-  xpQuery = "//a/@href['198502'=substring(., string-length(.) - 3)]"
-)
+#ESA SST
+#in kelvin
 
 
-#open avhrr dataset. sst climatology for 1985-2001
-#convert HDF files to TIF
-sds <- get_subdatasets("F:/whales/avhrr/month02_combined.hdf")
-gdal_translate(sds[4], dst_dataset = "F:/whales/avhrr/month02_combined_hdfout.tif")
+parent.folder <- "F:/whales/ESA_SST"
+allfolders <- list.dirs(parent.folder)
 
-r <- raster()
-extent(r) <- extent(-180,180,-90,90)
-r <- disaggregate(r,fact=24)
+#open the feb files
+#make all files into a brick 
+febfolders <- allfolders[c(3,6,9,12,15,18)]
+februaries <- raster()
+for(i in 1:length(febfolders)){
+  febyrfiles <- list.files(febfolders[[i]],full.names=T)
+  for(j in 1:length(febyrfiles)){
+     r <- raster(febyrfiles[[j]])
+     februaries <- stack(februaries,r)
+  }
+}
+#get the mean february SST
+esafeb <- calc(februaries,fun=function(x){mean(x,na.rm=T)},filename="F:/whales/ESA_SST/ESA_feb_mean.tif")
+esafebc <- esafeb-273.15
+writeRaster(esafebc, filename="F:/whales/ESA_SST/ESA_feb_meanC.tif")
 
-avhrrfeb <- raster("F:/whales/avhrr/month02_combined_hdfout.tif")
-avhrrFeb <- projectRaster(avhrrfeb,r,method='bilinear',filename="F:/whales/avhrr/month02_combined.tif",overwrite=T)
+#open the aug files
+#make all files into a brick 
+augfolders <- allfolders[c(4,7,10,13,16,19)]
+augusts <- raster()
+for(i in 1:length(augfolders)){
+  augyrfiles <- list.files(augfolders[[i]],full.names=T)
+  for(j in 1:length(augyrfiles)){
+    r <- raster(augyrfiles[[j]])
+    augusts <- stack(augusts,r)
+  }
+}
+#get the mean august SST
+esaaug <- calc(augusts,fun=function(x){mean(x,na.rm=T)},filename="F:/whales/ESA_SST/ESA_aug_mean.tif")
+esaaugc <- esaaug-273.15
+writeRaster(esaaugc, filename="F:/whales/ESA_SST/ESA_aug_meanC.tif")
 
-sds <- get_subdatasets("F:/whales/avhrr/month08_combined.hdf")
-gdal_translate(sds[4], dst_dataset = "F:/whales/avhrr/month08_combined_hdfout.tif")
-
-avhrraug <- raster("F:/whales/avhrr/month08_combined_hdfout.tif")
-avhrrAug <- projectRaster(avhrraug,r,method='bilinear',filename="F:/whales/avhrr/month08_combined.tif",overwrite=T)
-
-##########
 
 
+
+
+
+
+
+
+###
+#pre-process all cmip6 datasets - regrid from curvilinear to rectilinear for easy comparison with AVHRR
+#use CDO in Terminal https://code.mpimet.mpg.de/projects/cdo/embedded/index.html#x1-6190002.12.1
+# see info: cdo sinfon in.nc
+# concatenate files: ncrcat 85.nc 86.nc 87.nc 88.nc 89.nc 8589.nc
+# remap with bilinear interpolation: cdo remapbil,r360x180 in.nc out.nc
+
+#trouble with IPSL ssp245 & ssp545
 
 
 
@@ -45,24 +70,19 @@ avhrrAug <- projectRaster(avhrraug,r,method='bilinear',filename="F:/whales/avhrr
 ###hist 
 
 #open each cmip6 model. get just the feb/august. 
-parent.folder <- "F:/cmip6/tos/hist"
+parent.folder <- "F:/cmip6/tos/regrid/hist"
 modelnames <- c("ACCESS-CM2","ACCESS-ESM1-5","CAMS-CSM1-0","CanESM5","CAS-ESM2-0","CESM2-WACCM","CIESM","CMCC-CM2-SR5","CMCC-ESM2","FGOALS-f3-L",
                 "FGOALS-g3","FIO-ESM-2-0","GFDL-CM4","GFDL-ESM4","IPSL-CM6A-LR","MIROC6","MRI-ESM2-0","NESM3","NorESM2-LM","NorESM2-MM")
 febs <- c(2+12*(0:16)) #17 years between 1985-2001
 augs <- c(8+12*(0:16))
 
-#skip #5 CAS-ESM2-0 because not equally spaced grid
-#skip #13,14,16,17 GFDL-CM4, GFDL-ESM4, MIROC6,MRI-ESM2-0 for same reason
+
 for(i in 1:length(modelnames)){
   modelfiles <- list.files(parent.folder, pattern=modelnames[i],full.names=T)
-  #some models are broken up into multiple files. make model into single brick with all years
-  r <- raster()
-  for(j in 1:length(modelfiles)){
-    modelbrick <- brick(modelfiles[j],varname="tos")
-    r <- stack(r,modelbrick)
-  }
+  #models are concatenated in one file. open full dataset file
+  r <- brick(modelfiles[1],varname="tos")
   #keep just the years 1985-2001
-  r8501 <- dropLayer(r,c(1:1620,(nlayers(r)-155):nlayers(r))) #CHECK THIS
+  r8501 <- dropLayer(r,c(1:1620,(nlayers(r)-155):nlayers(r)))
   #extract just the febs
   febstack <- stack(r8501[[febs[1:17]]])
   #calculate mean feb temp for 1985-2001 (base period) for each model. save
@@ -80,23 +100,19 @@ for(i in 1:length(modelnames)){
 ###ssp245
 
 #open each cmip6 model. get just the feb/august. 
-parent.folder <- "F:/cmip6/tos/ssp245"
-modelnames <- c("ACCESS-CM2","ACCESS-ESM1-5","CAMS-CSM1-0","CanESM5","CESM2-WACCM","CIESM","CMCC-CM2-SR5","CMCC-ESM2","FGOALS-f3-L",
-                "FGOALS-g3","FIO-ESM-2-0","IPSL-CM6A-LR","NESM3","NorESM2-LM","NorESM2-MM")
-#skip #5 CAS-ESM2-0 because not equally spaced grid
-#skip #13,14,16,17 GFDL-CM4, GFDL-ESM4, MIROC6,MRI-ESM2-0 for same reason
+parent.folder <- "F:/cmip6/tos/regrid/ssp245"
+modelnames <- c("ACCESS-CM2","ACCESS-ESM1-5","CAMS-CSM1-0","CanESM5","CAS-ESM2-0","CESM2-WACCM","CIESM","CMCC-CM2-SR5","CMCC-ESM2","FGOALS-f3-L",
+                "FGOALS-g3","FIO-ESM-2-0", "GFDL-CM4", "GFDL-ESM4", "MIROC6", "MRI-ESM2-0","NESM3","NorESM2-LM","NorESM2-MM")
+#skip "IPSL-CM6A-LR" - could not regrid
 
 febs <- c(2+12*(0:85)) #86 years between 2015-2100 
 augs <- c(8+12*(0:85))
 
+#run CAMS-CSM1-0 separately - only goes up to 2099, not 2100. use febstack <- stack(r[[febs[1:85]]]) and augstack <- stack(r[[augs[1:86]]])
 for(i in 1:length(modelnames)){
   modelfiles <- list.files(parent.folder, pattern=modelnames[i],full.names=T)
   #make single brick with all model years 2015-2100
-  r <- raster()
-  for(j in 1:length(modelfiles)){
-    modelbrick <- brick(modelfiles[j],varname="tos")
-    r <- stack(r,modelbrick)
-  }
+  r <- brick(modelfiles[1],varname="tos")
   #extract just the febs. this will be feb temps 2015-2100 (rasterbrick with 86 layers)
   febstack <- stack(r[[febs[1:86]]])
   #calculate mean decadal temps (e.g. 2015-2024, 2025-2034, ...). will result in a 9 layer stack
@@ -132,17 +148,26 @@ for(i in 1:length(modelnames)){
                       filename=paste0("F:/whales/cmip6/ssp245/",modelnames[i],"_decadedelta_aug.tif"))
 }
 
-extent(rtemplate) <- extent(0,360,-90,90)
+
 #calculate model median delta
 #for feb
 febmodelfiles <- list.files("F:/whales/cmip6/ssp245",pattern="feb",full.names=T)
 for(i in 1:length(febmodelfiles)){
   object <- paste0("feb245.", i)
+  r <- brick(febmodelfiles[i],varname="tos")
   assign(object,r)
 }
 deltafeb <- overlay(feb245.1,feb245.2,feb245.3,feb245.4,feb245.5,feb245.6,feb245.7,feb245.8,feb245.9,feb245.10,
-        feb245.11,feb245.12,feb245.13,feb245.14,feb245.15,
-        fun=median,na.rm=T,filename="F:/whales/cmip6/ssp245/modelmedian_decadedelta_feb.tif")
+        feb245.11,feb245.12,feb245.13,feb245.14,feb245.15,feb245.16,feb245.17,feb245.18,feb245.19,
+        fun=function(x){median(x,na.rm=T)},filename="F:/whales/cmip6/ssp245/modelmedian_decadedelta_feb.tif")
+deltafebd <- disaggregate(deltafeb, fact=20)
+i_brick1 <- crop(deltafebd,extent(180,359.5,-90,90))
+i_brick2 <- crop(deltafebd,extent(-0.5,180,-90,90))
+extent(i_brick1) <- c(-180,-0.5,-90,90)
+deltafebr <- merge(i_brick1,i_brick2)
+writeRaster(deltafebr,filename="F:/whales/cmip6/ssp245/modelmedian_decadedelta_febr.tif")
+
+
 #for aug
 augmodelfiles <- list.files("F:/whales/cmip6/ssp245",pattern="aug",full.names=T)
 for(i in 1:length(augmodelfiles)){
@@ -151,15 +176,23 @@ for(i in 1:length(augmodelfiles)){
   assign(object,r)
 }
 deltaaug <- overlay(aug245.1,aug245.2,aug245.3,aug245.4,aug245.5,aug245.6,aug245.7,aug245.8,aug245.9,aug245.10,
-        aug245.11,aug245.12,aug245.13,aug245.14,aug245.15,aug245.16,aug245.17,aug245.18,aug245.19,aug245.20,
-        fun=median,na.rm=T,filename=paste0("F:/whales/cmip6/ssp245/modelmedian_decadedelta_aug.tif"))
+        aug245.11,aug245.12,aug245.13,aug245.14,aug245.15,aug245.16,aug245.17,aug245.18,aug245.19,
+        fun=function(x){median(x,na.rm=T)},filename="F:/whales/cmip6/ssp245/modelmedian_decadedelta_aug.tif")
+deltaaugd <- disaggregate(deltaaug, fact=20)
+i_brick1 <- crop(deltaaugd,extent(180,359.5,-90,90))
+i_brick2 <- crop(deltaaugd,extent(-0.5,180,-90,90))
+extent(i_brick1) <- c(-180,-0.5,-90,90)
+deltaaugr <- merge(i_brick1,i_brick2)
+writeRaster(deltaaugr,filename="F:/whales/cmip6/ssp245/modelmedian_decadedelta_augr.tif")
+
+
 
 
 #add delta to avhrr to get downscaled avhrr sst
 #for feb
-febsst245 <- overlay(avhrrFeb,deltafeb, fun=sum,filename="F:/whales/cmip6/ssp245/FebSST_ssp245.tif")
+febsst245 <- overlay(esafebc,deltafebr, fun=sum,filename="F:/whales/cmip6/ssp245/FebSST_ssp245.tif")
 #for aug
-augsst245 <- overlay(avhrrAug,deltaaug, fun=sum,filename="F:/whales/cmip6/ssp245/AugSST_ssp245.tif")
+augsst245 <- overlay(esaaugc,deltaaugr, fun=sum,filename="F:/whales/cmip6/ssp245/AugSST_ssp245.tif")
 
 
 
@@ -169,22 +202,21 @@ augsst245 <- overlay(avhrrAug,deltaaug, fun=sum,filename="F:/whales/cmip6/ssp245
 ###ssp585
 
 #open each cmip6 model. get just the feb/august. 
-parent.folder <- "F:/cmip6/tos/ssp585"
+parent.folder <- "F:/cmip6/tos/regrid/ssp585"
 modelnames <- c("ACCESS-CM2","ACCESS-ESM1-5","CAMS-CSM1-0","CanESM5","CAS-ESM2-0","CESM2-WACCM","CIESM","CMCC-CM2-SR5","CMCC-ESM2","FGOALS-f3-L",
-                "FIO-ESM-2-0","GFDL-CM4","GFDL-ESM4","IPSL-CM6A-LR","MIROC6","MRI-ESM2-0","NESM3","NorESM2-LM","NorESM2-MM")
-febs <- c(2+12*(0:86)) #86 years between 2015-2100 
-augs <- c(8+12*(0:86))
+                "FIO-ESM-2-0","GFDL-CM4","GFDL-ESM4","MIROC6","MRI-ESM2-0","NESM3","NorESM2-LM","NorESM2-MM")
+#skip "IPSL-CM6A-LR" - could not regrid
 
-for(i in 1:length(modelnames)){
+febs <- c(2+12*(0:85)) #86 years between 2015-2100 
+augs <- c(8+12*(0:85))
+
+#run CAMS-CSM1-0 separately - only goes up to 2099, not 2100. use febstack <- stack(r[[febs[1:85]]]) and augstack <- stack(r[[augs[1:86]]])
+for(i in 16:length(modelnames)){
   modelfiles <- list.files(parent.folder, pattern=modelnames[i],full.names=T)
   #make single brick with all model years 2015-2100
-  r <- raster()
-  for(j in 1:length(modelfiles)){
-    modelbrick <- brick(modelfiles[j],varname="tos")
-    r <- stack(r,modelbrick)
-  }
+  r <- brick(modelfiles[1],varname="tos")
   #extract just the febs. this will be feb temps 2015-2100 (rasterbrick with 86 layers)
-  febstack <- stack(r[[febs]])
+  febstack <- stack(r[[febs[1:86]]])
   #calculate mean decadal temps (e.g. 2016-2025, 2026-2035, ...). will result in a 9 layer stack
   index <- c(1:nlayers(febstack))
   febdecadal <- raster()
@@ -201,7 +233,7 @@ for(i in 1:length(modelnames)){
           filename=paste0("F:/whales/cmip6/ssp585/",modelnames[i],"_decadedelta_feb.tif"))
   
   #extract just the augs. this will be aug temps 2015-2100 (rasterbrick with 86 layers)
-  augstack <- stack(r[[augs]])
+  augstack <- stack(r[[augs[1:86]]])
   #calculate mean decadal temps (e.g. 2016-2025, 2026-2035, ...). will result in a 9 layer stack
   index <- c(1:nlayers(augstack))
   augdecadal <- raster()
@@ -227,8 +259,15 @@ for(i in 1:length(febmodelfiles)){
   assign(object,r)
 }
 deltafeb <- overlay(feb585.1,feb585.2,feb585.3,feb585.4,feb585.5,feb585.6,feb585.7,feb585.8,feb585.9,feb585.10,
-                    feb585.11,feb585.12,feb585.13,feb585.14,feb585.15,feb585.16,feb585.17,feb585.18,feb585.19,feb585.20,
-                    fun=median,na.rm=T,filename="F:/whales/cmip6/ssp585/modelmedian_decadedelta_feb.tif")
+                    feb585.11,feb585.12,feb585.13,feb585.14,feb585.15,feb585.16,feb585.17,feb585.18,
+                    fun=function(x){median(x,na.rm=T)},filename="F:/whales/cmip6/ssp585/modelmedian_decadedelta_feb.tif")
+deltafebd <- disaggregate(deltafeb, fact=20)
+i_brick1 <- crop(deltafebd,extent(180,359.5,-90,90))
+i_brick2 <- crop(deltafebd,extent(-0.5,180,-90,90))
+extent(i_brick1) <- c(-180,-0.5,-90,90)
+deltafebr <- merge(i_brick1,i_brick2)
+writeRaster(deltafebr,filename="F:/whales/cmip6/ssp585/modelmedian_decadedelta_febr.tif")
+
 #for aug
 augmodelfiles <- list.files("F:/whales/cmip6/ssp585",pattern="aug",full.names=T)
 for(i in 1:length(augmodelfiles)){
@@ -237,15 +276,21 @@ for(i in 1:length(augmodelfiles)){
   assign(object,r)
 }
 deltaaug <- overlay(aug585.1,aug585.2,aug585.3,aug585.4,aug585.5,aug585.6,aug585.7,aug585.8,aug585.9,aug585.10,
-                    aug585.11,aug585.12,aug585.13,aug585.14,aug585.15,aug585.16,aug585.17,aug585.18,aug585.19,aug585.20,
-                    fun=median,na.rm=T,filename=paste0("F:/whales/cmip6/ssp585/modelmedian_decadedelta_aug.tif"))
+                    aug585.11,aug585.12,aug585.13,aug585.14,aug585.15,aug585.16,aug585.17,aug585.18,
+                    fun=function(x){median(x,na.rm=T)},filename=paste0("F:/whales/cmip6/ssp585/modelmedian_decadedelta_aug.tif"))
+deltaaugd <- disaggregate(deltaaug, fact=20)
+i_brick1 <- crop(deltaaugd,extent(180,359.5,-90,90))
+i_brick2 <- crop(deltaaugd,extent(-0.5,180,-90,90))
+extent(i_brick1) <- c(-180,-0.5,-90,90)
+deltaaugr <- merge(i_brick1,i_brick2)
+writeRaster(deltaaugr,filename="F:/whales/cmip6/ssp585/modelmedian_decadedelta_augr.tif")
 
 
-#add delta to avhrr to get downscaled avhrr sst
+#add delta to esa to get downscaled esa sst
 #for feb
-febsst585 <- overlay(avhrrfeb,deltafeb, fun=sum,filename="F:/whales/cmip6/ssp585/FebSST_ssp585.tif")
+febsst585 <- overlay(esafebc,deltafebr, fun=sum,filename="F:/whales/cmip6/ssp585/FebSST_ssp585.tif")
 #for aug
-augsst585 <- overlay(avhrraug,deltaaug, fun=sum,filename="F:/whales/cmip6/ssp585/AugSST_ssp585.tif")
+augsst585 <- overlay(esaaugc,deltaaugr, fun=sum,filename="F:/whales/cmip6/ssp585/AugSST_ssp585.tif")
 
 
 
@@ -354,3 +399,34 @@ temp2100 <- overlay(cmipdiffmd,noaa.mmm,fun=sum,na.rm=T,filename="cmip.rcp85.210
 #for each historical model, get feb means for 1985-1990 + 1993 (for comparison against NOAA). calculate model mean and median
 
 #taylor diagram - justify the model median approach. compare historical model sst to noaa sst for 2015
+
+
+
+###avhrr
+
+
+#open avhrr dataset. sst climatology for 1985-2001
+#convert HDF files to TIF
+# sds <- get_subdatasets("F:/whales/avhrr/month02_combined.hdf")
+# gdal_translate(sds[1], dst_dataset = "F:/whales/avhrr/month02_combined_band0.tif")
+# 
+# avhrrfeb <- raster("F:/whales/avhrr/month02_combined_band5.tif",
+# crs="+init=epsg:4267 +proj=longlat +ellps=clrk66 +datum=NAD27
+# +no_defs")
+# 
+# sds <- get_subdatasets("F:/whales/avhrr/month08_combined.hdf")
+# gdal_translate(sds[1], dst_dataset = "F:/whales/avhrr/month08_combined_band0.tif")
+# 
+# avhrraug <- raster("F:/whales/avhrr/month08_combined_band0.tif")
+
+
+# inpath <- "D:/climatologydata/r"
+# setwd(inpath)
+# filenames <- list.files(,pattern = ".hdf$", full.names = F)
+# for(filename in filenames)
+# {
+#   sds <- get_subdatasets(filename)
+#   gdal_translate(sds[1],dst_dataset = paste0(substr(filename, 1, nchar(filename)-4),".tif"))
+# }
+
+##########
